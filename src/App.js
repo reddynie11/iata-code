1505fbd55a7c2690764677cac1182f8600dbf9cb
reddynie11 <reddy.nie11@gmail.com>
2019-10-22 22:12:58 +0530

    airport details component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Header from './components/header/header.component';
 import MainPage from './components/mainpage/mainpage.component';
+import airportPage from './components/airportPage/airportPage.component';
 
 // import {airportData} from '../../data';
 
@@ -24,6 +25,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={MainPage} />
+        <Route path='/:id' component={airportPage } />
       </Switch>
       
     </div>
--- src/components/airportPage/airportPage.component.js
+++ src/components/airportPage/airportPage.component.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const airportPage = ()=>{
+const airportPage = (props)=>{
+    console.log(props)
     return(
-        <div>
-            
+        <div className='detail-page'>
+            <img className='detail-background' src={require(`../images/${props.airport.id}.jpg`)} />  
+            <div className='deatil-content'>
+                <h1 className='port-id'>{props.airport.id.toUpperCase()}</h1>
+                <h2 className='port-name'>{props.airport.name.toUpperCase()}</h2>
+                <h3>{props.airport.city.toUpperCase()}, {props.airport.country.toUpperCase()}</h3>
+                <p>{props.airport.description}</p>
+            </div>         
         </div>
     )
 }
+
+const mapStateToProps = (state, props)=>{
+    let ID = props.match.params.id;
+    return{
+        airport : state.find((item)=>{
+            return item.id == ID;
+        })
+    }
+}
+
+export default connect(mapStateToProps)(airportPage);
