003cff8e0fc7d41611dbae2c401362403c19bf2c
reddynie11 <reddy.nie11@gmail.com>
2019-10-23 18:39:59 +0530

    m

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -4,7 +4,7 @@ import { Navbar, Form, FormControl } from 'react-bootstrap';
 const Header = ({search})=>{
     return(
         <Navbar bg="info" variant="dark">
-            <Navbar.Brand className='mr-auto ml-5' href="#">IATA airport codes</Navbar.Brand>
+            <Navbar.Brand className='mr-auto ml-5' href="#">AirportWiki</Navbar.Brand>
             <Form inline className='mr-5 my-2'>
                 <Form.Control type="search" placeholder="Search airport" className="mr-sm-3" onChange={search} />
             </Form>
